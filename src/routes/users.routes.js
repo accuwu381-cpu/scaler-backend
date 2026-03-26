8a22df3764b37be2da160776b271dfb7347248dd
Ritesh Prajapati <prajapatiritesh381@gmail.com>
2026-03-27 04:39:13 +0530

    bug fix

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const { verifyToken } = require("../middlewares/auth.middleware");
-const { getAllUsers } = require("../controllers/users.controller");
-
 const router = express.Router();
 const { getAllUsers, pingUser } = require("../controllers/users.controller");
 
